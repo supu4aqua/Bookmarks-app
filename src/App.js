dcfcd24c858e70f685d3b2ad4c54511df3c7112e
Supreet <me.supi@gmail.com>
2020-01-12 22:06:25 -0500

    Completed Routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import AddBookmark from './AddBookmark/AddBookmark';
 import BookmarkList from './BookmarkList/BookmarkList';
 import Nav from './Nav/Nav';
@@ -31,20 +32,18 @@ const bookmarks = [
 
 class App extends Component {
   state = {
-    page: 'list',
+
     bookmarks,
     error: null,
   };
 
-  changePage = (page) => {
-    this.setState({ page })
-  }
+
 
   setBookmarks = bookmarks => {
     this.setState({
       bookmarks,
       error: null,
-      page: 'list',
+
     })
   }
 
@@ -73,23 +72,30 @@ class App extends Component {
   }
 
   render() {
-    const { page, bookmarks } = this.state
+
     return (
       <main className='App'>
         <h1>Bookmarks!</h1>
-        <Nav clickPage={this.changePage} />
+      <Nav />
         <div className='content' aria-live='polite'>
-          {page === 'add' && (
-            <AddBookmark
-              onAddBookmark={this.addBookmark}
-              onClickCancel={() => this.changePage('list')}
+        <Route
+         path='/add-bookmark'
+         render={({ history }) => {
+
+             return <AddBookmark
+               onAddBookmark={this.addBookmark}
+              onClickCancel={() => history.push('/')}
             />
-          )}
-          {page === 'list' && (
-            <BookmarkList
-              bookmarks={bookmarks}
-            />
-          )}
+          }}
+         />
+         <Route
+            exact
+            path='/'
+            render={() =>
+                            <BookmarkList
+                 bookmarks={bookmarks}
+              />}
+          />
         </div>
       </main>
     );
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_KEY: '$2a$10$9jYMfCL85.bJ20NDRPbww.G0qk.kT/pWvMD84KJj8LA5oMIHAsCmi',
 }
